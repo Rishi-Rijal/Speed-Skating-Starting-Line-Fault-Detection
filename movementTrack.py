e6d2a9d3f4d0696debd9a43890775d516e364a9c
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-05-01 19:04:20 +0300

    chaged landmarks and movements list to store landmarks and movement for different frames from plain variables

--- movementTrack.py
+++ movementTrack.py
@@ -10,20 +10,17 @@ mp_drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0) #capturing the video default webcam
 
-prev_landmarks1 = None
-prev_landmarks2 = None
-prev_landmarks3 = None
-prev_landmarks4 = None
-prev_landmarks5 = None
+
+#landmarks that are used to store last 5 landmaarks
+landMarks = []
+
+
 
 movement_threshold = 0.02
-movement = 0
-sec_movement = 0
-thi_movement = 0
-fou_movement = 0
-fiv_movement = 0
 
-prev_landmarks = None
+#list to store current movements according to previous landmarks
+movements = []
+
 
 trackMovement = 0
 
@@ -46,59 +43,39 @@ while cap.isOpened():
     result = pose.process(frame_rgb)
 
     if result.pose_landmarks:
-        mp_drawing.draw_landmarks(frame,result.pose_landmarks,mp_pose.POSE_CONNECTIONS)
-
-        landmarks = np.array([
-            (result.pose_landmarks.landmark[i].x,
-             result.pose_landmarks.landmark[i].y,
-             result.pose_landmarks.landmark[i].z)
-            for i in key_indices
+          mp_drawing.draw_landmarks(frame,result.pose_landmarks,mp_pose.POSE_CONNECTIONS)
+     
+          #storing current landmarks
+          currLandmarks = np.array([
+               (result.pose_landmarks.landmark[i].x,
+               result.pose_landmarks.landmark[i].y,
+               result.pose_landmarks.landmark[i].z)
+               for i in key_indices
         ])
 
-        if prev_landmarks1 is not None:
-            movement = np.linalg.norm(landmarks - prev_landmarks1, axis=1).mean()
 
-        if prev_landmarks2 is not None:
-             sec_movement = np.linalg.norm(landmarks - prev_landmarks2, axis=1).mean()
-        
-        if prev_landmarks3 is not None:
-             thi_movement = np.linalg.norm(landmarks - prev_landmarks3, axis=1).mean()
-        
-        if prev_landmarks4 is not None:
-             fou_movement = np.linalg.norm(landmarks - prev_landmarks4, axis=1).mean()
+          #caclulating movements with respect of last 5 landmarks and adding to movements
+          for index, landmark in enumerate(landMarks, start=0):
+               if landmark is not None:
+                    currMovement = np.linalg.norm(currLandmarks - landmark, axis=1).mean()
+                    movements.append(currMovement)
 
-        if prev_landmarks5 is not None:
-             fiv_movement = np.linalg.norm(landmarks - prev_landmarks5, axis=1).mean()
-        
-    
-        if movement > movement_threshold:
-                print(f"a movement here! {trackMovement}")
-                trackMovement += 1
-                cv2.putText(frame, 'Significant Movement!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
-                            1, (0, 255, 0), 2, cv2.LINE_AA)
-        
-        if sec_movement > movement_threshold:
-             cv2.putText(frame, 'Significant Movement!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
-                            1, (0, 255, 0), 2, cv2.LINE_AA)
+          for movement in movements:
+               if movement > movement_threshold:
+                    print(f"a movement here! {trackMovement}")
+                    trackMovement += 1
+                    cv2.putText(frame, 'Significant Movement!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
+                              1, (0, 255, 0), 2, cv2.LINE_AA)
 
-        if thi_movement > movement_threshold:
-             cv2.putText(frame, 'Significant Movement!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
-                            1, (0, 255, 0), 2, cv2.LINE_AA)
+          landMarks_size = len(landMarks)
+          i = landMarks_size -1
 
-        if fou_movement > movement_threshold:
-             cv2.putText(frame, 'Significant Movement!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
-                            1, (0, 255, 0), 2, cv2.LINE_AA)
+          #shifting the landmarks data acc to new frame
+          if len(landMarks) >= 5:
+               landMarks.pop()  # Remove the oldest landmark set
+          landMarks.insert(0, currLandmarks)  # Add the latest landmarks at the beginning
 
-        if fiv_movement > movement_threshold:
-             cv2.putText(frame, 'Significant Movement!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 
-                            1, (0, 255, 0), 2, cv2.LINE_AA)
-
-        prev_landmarks5 = prev_landmarks4    
-        prev_landmarks4 = prev_landmarks3
-        prev_landmarks3 = prev_landmarks2
-        prev_landmarks2 = prev_landmarks1
-        prev_landmarks1 = landmarks
-        #prev_landmarks = landmarks
+          
     cv2.imshow('Filtered Movement Detection', frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break

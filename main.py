9d0b69d55b96914f94f4c4a8083e40add2d9b639
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-05-08 18:56:41 +0300

    removed unnecessary imports

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 #importing different modules
 import cv2
-import mediapipe as mp
 import numpy as np
 from movement import isMovement
 
-
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
-mp_drawing = mp.solutions.drawing_utils
-
 cap = cv2.VideoCapture(0) #capturing the video default webcam
-
 landMarks = [] #landmarks -> that are used to store last 5 landmarks
-
 movement_threshold = 0.02
-
 movements = [0,0,0,0,0] #list to store current movements according to previous landmarks
 
-trackMovement = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
-
     isMovement(frame,landMarks, movements, movementThreshold=movement_threshold)
           
     cv2.imshow('Filtered Movement Detection', frame)

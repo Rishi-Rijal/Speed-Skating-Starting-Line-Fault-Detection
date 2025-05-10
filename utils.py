9ba8a9b23f2378ad1ae8b2554583520918016ad8
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-05-10 13:52:42 +0300

    updated the line width

--- utils.py
+++ utils.py
@@ -17,7 +17,7 @@ def get_landmark_y(landmarks, frame, landmark_id):
     return int(landmarks[landmark_id.value][1] * frame.shape[0])
 
 def draw_lines(frame, pre_start_y, start_y, state):
-    cv2.line(frame, (0, pre_start_y), (frame.shape[1], pre_start_y), (255, 0, 0), 5)
+    cv2.line(frame, (0, pre_start_y), (frame.shape[1], pre_start_y), (255, 0, 0), 2)
     cv2.line(frame, (0, start_y), (frame.shape[1], start_y), (0, 255, 0), 2)
     cv2.putText(frame, f'State: {state}', (10, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
